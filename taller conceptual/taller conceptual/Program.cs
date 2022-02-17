953da58b6d38cf39bcdf0e1dff9ececca8db433a
lozano1213 <99831008+lozano1213@users.noreply.github.com>
2022-02-17 17:44:43 -0500

    Add files via upload

---
+++ taller conceptual/taller conceptual/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+/// <summary>
+/// Santiago Lozano
+/// taller conceptual
+/// Sueldo 
+/// </summary>
+namespace taller_conceptual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iTituloUniversitario;
+            double bSueldo;
+            double bQuincena =1050000;
+            double bQuncena2;
+            double bSeguroColectivo;
+            double bEPS;
+            double bParoForzoso;
+            double bCajaCompensacion;
+            double bBonoAlimentacion;
+            int iDiasLaborados;
+            double bBonoTransporte =120000;
+
+            Console.WriteLine("cuenta con un titulo universitario? (1 = SI / 2 = NO)");
+            iTituloUniversitario = Convert.ToInt32(Console.ReadLine());
+            if (iTituloUniversitario ==1)
+            {
+                bQuncena2 = bQuincena * 0.1;
+            }
+            else
+            {
+                bQuncena2 = bQuincena * 0;
+            }
+
+            Console.WriteLine("¿cuantos dias trabajó?");
+            iDiasLaborados = Convert.ToInt32(Console.ReadLine());
+            
+            bBonoAlimentacion = iDiasLaborados * 25000;
+            bEPS = bQuincena * 0.02;
+            bParoForzoso = bQuincena * 0.01;
+            bCajaCompensacion = bQuincena * 0.02;
+            bSeguroColectivo = bQuincena * 0.02;
+            
+            bSueldo = bQuincena + bQuncena2 + bBonoTransporte + bBonoAlimentacion -bEPS-bParoForzoso-bSeguroColectivo-bCajaCompensacion;
+            
+            Console.WriteLine("su sueldo quincenal es de"+bSueldo+"$");
+            
+           
+            Console.ReadKey();
+             
+               
+        }
+    }
+}
